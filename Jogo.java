387fce098e0c0e8291b2feee4acddabf85462fbb
Lara Raffel <lara.b.r@outlook.com>
2022-11-14 19:59:49 -0300

    apostila02(introducao_poo): implementacao de mais um personagem junto de mais ações dos jogadores

--- Jogo.java
+++ Jogo.java
@@ -1,15 +1,25 @@
 public class Jogo {
     public static void main(String[] args) throws Exception{
-        Personagem p = new Personagem("Lucas", 5, 5, 5);
+        Personagem p1 = new Personagem("Lucas", 5, 5, 5);
         Personagem p2 = new Personagem ("Lara", 10, 0, 0);
+        Personagem p3 = new Personagem ("Yasmin", 1, 4, 3);
 
     while (true){
-        p.cacar();
-        p.comer();
-        p.dormir();
-        p.cacar();
-        p.cacar();
-        p.cacar();
+        p1.cacar();
+        p1.comer();
+        p3.dormir();
+        p3.comer();
+        p2.comer();
+        p3.cacar();
+        p3.cacar();
+        p3.dormir();
+        p2.cacar();
+        p2.cacar();
+        p2.dormir();
+        p1.dormir();
+        p1.cacar();
+        p1.cacar();
+        p1.cacar();
         System.out.println("=============================");
         Thread.sleep(5000);
         }
